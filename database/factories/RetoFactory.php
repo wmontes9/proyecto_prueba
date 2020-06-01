02316eb3b15b1f163167b9d5f38761d17bb64130
wmontes9 <43587256+wmontes9@users.noreply.github.com>
2020-06-01 17:06:02 -0500

    Initial commit

---
+++ database/factories/RetoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Reto;
+use Faker\Generator as Faker;
+
+$factory->define(Reto::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
