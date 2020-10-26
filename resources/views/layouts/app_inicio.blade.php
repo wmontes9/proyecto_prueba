2f0502eaecaa5b609f478894644e2553b157e2ad
wmontes9 <43587256+wmontes9@users.noreply.github.com>
2020-10-26 18:21:20 -0500

    Ajuste pagina principal

--- resources/views/layouts/app_inicio.blade.php
+++ resources/views/layouts/app_inicio.blade.php
@@ -220,12 +220,12 @@
     <div id="app">
 		<div class="wrapper">
 			<div class="container-fluid">
-				<nav class="navbar navbar-expand-lg navbar-light" id="navbar">
+				<!---<nav class="navbar navbar-expand-lg navbar-light" id="navbar">
                 <div class="log">
                 <img src="{{url('img/innexsa.png')}}" class="img-fluid">
-                </div>	
+                </div>-->	
 					<!--<a class="navbar-brand" href="#">Navbar</a> -->
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
 					</button>
 					<div class="collapse navbar-collapse" id="navbarNav">
@@ -247,7 +247,7 @@
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="#contactenos">Contacto</a>
-                        </li>
+                        </li>-->
                         <!--<li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
                               Dropdown link
@@ -265,6 +265,7 @@
                         {{-- <li>
                             <a href="{{ url('/register') }}" class="nav-link"> Registrarse</a>
                         </li> --}}
+                    <!--
                         <li>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</li>
                         <li>
                         <a href="#login" data-toggle="modal" class="nav-link" > Iniciar Sesión</a>
@@ -294,7 +295,7 @@
 						</ul>
 					</div>
 					
-				</nav>
+				</nav>-->
                 <main class="py-4">
                  @yield('content')
                     <div id="app-login">
